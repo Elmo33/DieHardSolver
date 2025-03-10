d43aa690799b8646bfce68f4b4336661e03017e2
saba <smomtselidze@altinity.com>
2025-03-10 15:37:55 +0400

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.memory import ConversationBufferMemory
 
-model_path = "/root/DieHardSolver/qwen2.5-14b-instruct-fp16.gguf"
+model_path = "qwen2.5-14b-instruct-fp16-00001-of-00008.gguf"
 
 
 # Load the Llama model using LangChain's LlamaCpp wrapper
